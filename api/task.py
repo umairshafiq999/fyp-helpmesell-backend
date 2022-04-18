f73b1b3d46c5d7dce539c7c9d84fc19a73a70a8a
Umair Shafiq <umairshafiq380@gmail.com>
2022-04-18 10:05:51 +0500

    added more

--- api/task.py
+++ api/task.py
@@ -19,34 +19,34 @@ def DataCleaningOfShophive(price):
     price.product_price.replace('Special Price', '').replace(' ', '')
 
 
-@shared_task
-def LocalSellerFileUpload(file):
-    fileSheet = pandas.read_excel(file, sheet_name=0, index_col=0, header=0)
-
-    for row in fileSheet.iterrows():
-        try:
-            product = Product.objects.get(product_name=row[0])
-            Price.objects.create(
-                product=product,
-                reference_site="shophive.com",
-                product_price=row[1]
-            )
-        except Product.DoesNotExist:
-            product = Product.objects.create(
-                product_name=row[0],
-                product_description='Great Phone',
-                product_image="https://www.apple.com/newsroom/images/product/iphone/standard/Apple_announce-iphone12pro_10132020.jpg.landing-big_2x.jpg",
-                min_price=20000,
-                max_price=30000,
-                offered_by=3
-
-            )
-            Price.objects.create(
-                product_id=product.id,
-                reference_site="shophive.com",
-                product_price=row[1]
-
-            )
+# @shared_task
+# def LocalSellerFileUpload(file):
+    # fileSheet = pandas.read_excel(file, sheet_name=0, index_col=0, header=0)
+    #
+    # for row in fileSheet.iterrows():
+    #     try:
+    #         product = Product.objects.get(product_name=row[0])
+    #         Price.objects.create(
+    #             product=product,
+    #             reference_site="shophive.com",
+    #             product_price=row[1]
+    #         )
+    #     except Product.DoesNotExist:
+    #         product = Product.objects.create(
+    #             product_name=row[0],
+    #             product_description='Great Phone',
+    #             product_image="https://www.apple.com/newsroom/images/product/iphone/standard/Apple_announce-iphone12pro_10132020.jpg.landing-big_2x.jpg",
+    #             min_price=20000,
+    #             max_price=30000,
+    #             offered_by=3
+    #
+    #         )
+    #         Price.objects.create(
+    #             product_id=product.id,
+    #             reference_site="shophive.com",
+    #             product_price=row[1]
+    #
+    #         )
     # print("Hello", file)
 
 
@@ -96,7 +96,7 @@ def ShopHiveScraper(url):
                 product = Product.objects.create(
                     product_name=row[0],
                     product_description='Great Phone',
-                    product_image=row[2]['src']
+                    product_image=row[2]['data-src']
 
                 )
                 Price.objects.create(
--- api/views.py
+++ api/views.py
@@ -192,7 +192,34 @@ class LocalSellerUploadedDataAPIView(APIView):
             data.save()
             file = settings.MEDIA_ROOT + data.ls_product_file.url[6:]
 
-            LocalSellerFileUpload.delay(file)
+            fileSheet = pandas.read_excel(file, sheet_name=0, index_col=0, header=0)
+
+            for row in fileSheet.iterrows():
+                try:
+                    product = Product.objects.get(product_name=row[0])
+                    Price.objects.create(
+                        product=product,
+                        reference_site="shophive.com",
+                        product_price=row[1]
+                    )
+                except Product.DoesNotExist:
+                    product = Product.objects.create(
+                        product_name=row[0],
+                        product_description='Great Phone',
+                        product_image="https://www.apple.com/newsroom/images/product/iphone/standard/Apple_announce-iphone12pro_10132020.jpg.landing-big_2x.jpg",
+                        min_price=20000,
+                        max_price=30000,
+                        offered_by=3
+
+                    )
+                    Price.objects.create(
+                        product_id=product.id,
+                        reference_site="shophive.com",
+                        product_price=row[1]
+
+                    )
+
+            # LocalSellerFileUpload.delay(file)
             return Response(serializer.data, status.HTTP_201_CREATED)
         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
