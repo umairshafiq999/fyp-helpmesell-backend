af410b7c7bd3318161acaa31dd5ea7baa8d6ce6c
Umair Shafiq <umairshafiq380@gmail.com>
2022-05-08 17:35:10 +0500

    fixed\

--- api/views.py
+++ api/views.py
@@ -268,7 +268,6 @@ class PackageAPIView(APIView):
 
 class PaymentAPIView(APIView):
     def post(self, request):
-        try:
             user = stripe.User.create(
                 username=request.data['name'],
                 payment_method=request.data['payment_method_id'],
@@ -277,7 +276,7 @@ class PaymentAPIView(APIView):
                     'default_payment_method': request.data['payment_method_id']
                 }
             )
-            subscription = stripe.Subscription.create(
+            stripe.Subscription.create(
                 user=user,
                 items=[
                     {
@@ -288,7 +287,5 @@ class PaymentAPIView(APIView):
                 ]
             )
 
-            return Response(subscription, status.HTTP_200_OK)
+            return Response("Payment Successful",status.HTTP_200_OK)
 
-        except:
-            return Response("Payment not successful", status.HTTP_500_INTERNAL_SERVER_ERROR)
