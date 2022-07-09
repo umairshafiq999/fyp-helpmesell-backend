51cf180e84583ddae9e12202b05e7b7a39a36e81
Umair Shafiq <umairshafiq380@gmail.com>
2022-07-09 18:01:08 +0500

    fixed

--- fyp/settings.py
+++ fyp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-%+!^xf6st9!-#36n3n&i1ls!h0e&&7^zb&b2e*q74xmmwxy6k*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
